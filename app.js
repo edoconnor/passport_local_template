82d68858727cf6ef94b339c3cec240d97d3467be
Ed O'Connor <ed@oconnor.app>
2022-07-18 14:20:23 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const session = require("express-session");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+// const User = require("./models/user");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connected to MonogoDB");
+});
+
+// mongoose.connection,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   };
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// PASSPORT
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new localStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: "Incorrect username." });
+
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (err) return done(err);
+        if (res === false)
+          return done(null, false, { message: "Incorrect password." });
+
+        return done(null, user);
+      });
+    });
+  })
+);
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+}
+
+function isLoggedOut(req, res, next) {
+  if (!req.isAuthenticated()) return next();
+  res.redirect("/");
+}
+
+// ROUTES
+
+app.get("/admin", isLoggedIn, (req, res) => {
+  res.render("admin");
+});
+
+app.get("/index",(req, res) => {
+  res.render("index");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login?error=true",
+  })
+);
+
+app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/index');
+    });
+  });
+
+// Setup our admin user
+app.get("/setup", async (req, res) => {
+  const exists = await User.exists({ username: "<pick_username>" });
+
+  if (exists) {
+    res.redirect("/login");
+    return;
+  }
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash("<pick_password>", salt, function (err, hash) {
+      if (err) return next(err);
+
+      const newAdmin = new User({
+        username: "admin",
+        password: hash,
+      });
+
+      newAdmin.save();
+
+      res.redirect("/login");
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on port", PORT);
+});
